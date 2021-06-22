b0cd7500a2186c868973532941a929d8b07dd54c
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-06-22 19:33:05 +0200

    init authentication app and config

--- projet12/settings.py
+++ projet12/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'crm.apps.CrmConfig',
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
