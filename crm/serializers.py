4a89b738c37125dbf30e43d6374d133e803e5221
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-06-24 21:23:32 +0200

    CRM serializers

---
+++ crm/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Client, Contract, Event, Status
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone', 'mobile', 'company_name', 'date_created', 'date_updated', 'sales_contact', 'active']
+        read_only_fields = ['id']
+
+class ContractSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contract
+        fields = ['sales_contact', 'client', 'date_created', 'date_updated', 'signed', 'amount', 'payment_due']
+        read_only_fields = ['id']
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['id', 'title', 'client', 'date_created', 'date_updated', 'support_contact', 'event_status', 'attendees', 'event_date', 'notes']
+        read_only_fields = ['id']
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = ['id', 'title', 'description']
+        read_only_fields = ['id']  
+        
