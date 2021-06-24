b366e7bfa7a66d02559ba6201866bf9e9cf42c22
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-06-24 15:46:16 +0200

    Configure admin site

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Client, Contract, Event, Status
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Contract)
+admin.site.register(Event)
+admin.site.register(Status)
