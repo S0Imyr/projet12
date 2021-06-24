1004bf34531c5aced0a06ef6997a0f3fec51e13f
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-06-24 17:26:06 +0200

    configure simple JWT and delete role (replace by django group)

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User, Role
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User)
-admin.site.register(Role)
+from .models import User
+
+
+admin.site.register(User, UserAdmin)
--- projet12/urls.py
+++ projet12/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+
+    path('api/login/', TokenObtainPairView.as_view(), name='login'),
+    path('api/login/refresh/', TokenRefreshView.as_view(), name='refresh'),
+
+    path('api/', include("crm.urls")),
+    path('api/', include("authentication.urls")),
 ]
