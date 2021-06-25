7367ea2df95ff673a5c580a64c9f51b637263234
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-06-25 18:08:12 +0200

    configure all viewset

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import ClientSerializer, ContractSerializer, EventSerializer, StatusSerializer
+from .models import Client, Contract, Event, Status
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Client instances.
+    """
+    serializer_class = ClientSerializer
+    queryset = Client.objects.all()
+
+
+class ContractViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Contract instances.
+    """
+    serializer_class = ContractSerializer
+    queryset = Contract.objects.all()
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Event instances.
+    """
+    serializer_class = EventSerializer
+    queryset = Event.objects.all()
+
+
+class StatusViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Status instances.
+    """
+    serializer_class = StatusSerializer
+    queryset = Status.objects.all()
--- projet12/urls.py
+++ projet12/urls.py
@@ -17,20 +17,21 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from authentication import views
+from authentication import views as authviews
+from crm import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'users', authviews.UserViewSet, basename='user')
+router.register(r'clients', views.ClientViewSet, basename='client')
+router.register(r'contracts', views.ContractViewSet, basename='contract')
+router.register(r'events', views.EventViewSet, basename='event')
+router.register(r'status', views.StatusViewSet, basename='status')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-
     path('token/', TokenObtainPairView.as_view(), name='token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
-
-    #path('api/', include("crm.urls")),
-    #path('api/', include("authentication.urls")),
 ]
