70b5d6adc6732b2dc3452811cd278e6485e9d4b6
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-07-24 16:48:09 +0200

    configure permission for client

--- crm/permissions.py
+++ crm/permissions.py
@@ -1,14 +1,37 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from django.shortcuts import get_object_or_404
-from .models import User, Group
+from authentication.models import User, Group
 
 
 class ClientPermission(BasePermission):
     def has_permission(self, request, view):
-        return super().has_permission(request, view)
+        if request.user.groups.filter(name="Guest").exists():
+            return False
+        else:
+            if request.method == 'POST':
+                if request.user.groups.filter(name="Management").exists() or \
+                request.user.groups.filter(name="Sales").exists():
+                    return True
+                else:
+                    return False
+            if request.method in SAFE_METHODS:
+                return True
 
     def has_object_permission(self, request, view, obj):
-        return super().has_object_permission(request, view, obj)
+        if request.user.groups.filter(name="Management").exists():
+            return True
+        elif request.user.groups.filter(name="Sales").exists():
+            if request.method in SAFE_METHODS:
+                return True
+            elif request.method == 'UPDATE':
+                return request.user in obj.sales_contact
+            else:
+                return False
+        elif request.user.groups.filter(name="Support").exists():
+            return request.method in SAFE_METHODS
+        else:
+            return False
+
 
 class ContractPermission(BasePermission):
     def has_permission(self, request, view):
@@ -23,4 +46,3 @@ class EventPermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return super().has_object_permission(request, view, obj)
-
