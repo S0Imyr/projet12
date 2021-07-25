c1c808f3cb0b1175fbcdbdca7b981696aea228f8
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-07-25 16:20:49 +0200

    configure permissions for contracts

--- crm/permissions.py
+++ crm/permissions.py
@@ -37,10 +37,34 @@ class ClientPermission(BasePermission):
 
 class ContractPermission(BasePermission):
     def has_permission(self, request, view):
-        return super().has_permission(request, view)
+        if request.user.groups.filter(name="Guest").exists():
+            return False
+        else:
+            if request.method in SAFE_METHODS:
+                return True
+            elif request.method == 'POST' or request.method == 'PUT':
+                if request.user.groups.filter(name="Management").exists() or \
+                request.user.groups.filter(name="Sales").exists():
+                    return True
+                else:
+                    return False
+            else:
+                return request.user.groups.filter(name="Management").exists()
 
     def has_object_permission(self, request, view, obj):
-        return super().has_object_permission(request, view, obj)
+        if request.user.groups.filter(name="Management").exists():
+            return True
+        elif request.user.groups.filter(name="Sales").exists():
+            if request.method in SAFE_METHODS:
+                return True
+            elif request.method == 'PUT':
+                return request.user == obj.sales_contact
+            else:
+                return False
+        elif request.user.groups.filter(name="Support").exists():
+            return request.method in SAFE_METHODS
+        else:
+            return False
 
 class EventPermission(BasePermission):
     def has_permission(self, request, view):
