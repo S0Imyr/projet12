29ecd95747cc1ab545cf6f23a1b0d211383d276e
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-07-26 00:51:53 +0200

    reconfigure permission for clients

--- crm/permissions.py
+++ crm/permissions.py
@@ -1,36 +1,48 @@
+from django.test import client
 from rest_framework.permissions import BasePermission, SAFE_METHODS
-from django.shortcuts import get_object_or_404
-from authentication.models import User, Group
 
+from crm.models import Contract, Event
 
 class ClientPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.user.groups.filter(name="Guest").exists():
-            return False
-        else:
-            if request.method in SAFE_METHODS:
+        if request.user.groups.filter(name="Management").exists():
+            return True
+        elif request.user.groups.filter(name="Sales").exists() or request.user.groups.filter(name="Support").exists():
+            if request.method == 'POST':
+                return request.user.groups.filter(name="Sales").exists()
+            elif request.method == 'PUT' or request.method == 'GET':
                 return True
-            elif request.method == 'POST' or request.method == 'PUT':
-                if request.user.groups.filter(name="Management").exists() or \
-                request.user.groups.filter(name="Sales").exists():
-                    return True
-                else:
-                    return False
             else:
-                return request.user.groups.filter(name="Management").exists()
+                return False
+        else:
+            return False
 
     def has_object_permission(self, request, view, obj):
         if request.user.groups.filter(name="Management").exists():
             return True
         elif request.user.groups.filter(name="Sales").exists():
-            if request.method in SAFE_METHODS:
-                return True
-            elif request.method == 'PUT':
-                return request.user == obj.sales_contact
+            if request.method == 'PUT' or request.method == 'GET':
+                if request.user == obj.sales_contact:
+                    return True
+                else:
+                    contracts = Contract.objects.filter(client=obj)
+                    sales_contacts = []
+                    for contract in contracts:
+                        sales_contacts.append(contract.sales_contact)
+                    print('Sales contact', sales_contacts)
+                    return request.user in sales_contacts
             else:
                 return False
         elif request.user.groups.filter(name="Support").exists():
-            return request.method in SAFE_METHODS
+            if request.method == 'GET':
+                events = Event.objects.filter(client=obj)
+                support_contacts = []
+                for event in events:
+                    support_contacts.append(event.support_contact)
+                print('Support contact', support_contacts)
+                return request.user in support_contacts
+            else:
+                return False
         else:
             return False
 
