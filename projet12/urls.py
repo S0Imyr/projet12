425766069863c5bc4362d30d3da2eb73869633fe
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-07-29 13:30:34 +0200

    sentry configuration

--- projet12/urls.py
+++ projet12/urls.py
@@ -14,11 +14,15 @@ router.register(r'contracts', views.ContractViewSet, basename='contract')
 router.register(r'events', views.EventViewSet, basename='event')
 router.register(r'status', views.StatusViewSet, basename='status')
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
+
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-
+    path('sentry-debug/', trigger_error),
     path('signup/', auth_views.Register.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
